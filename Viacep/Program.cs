5dc1dc3625b3d21c98cda4549e1ff8807d20249f
Luis Felipe <luis.s.mendes20@gmail.com>
2021-05-14 20:55:31 -0300

    Adicionando .gitignore Viacep

---
+++ Viacep/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+
+namespace Viacep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um Cep :");
+            string cep = Console.ReadLine();
+            string url = $"https://viacep.com.br/ws/" + cep + "/json/";
+
+            using (HttpClient http = new HttpClient()){
+
+                var response = http.GetAsync(url).GetAwaiter().GetResult();
+                var bodyResponse = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+
+                if (response.StatusCode != HttpStatusCode.OK) {
+                    Console.WriteLine($"Entrada de Cep Invalida. Cep Digitado: {0} ",cep);
+                    return;
+                }
+                Console.WriteLine(bodyResponse);
+
+            }
+        }
+    }
+}
