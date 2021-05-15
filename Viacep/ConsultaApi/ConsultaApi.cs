4836f9421b1f8e72c98a1d0882d9b26bfe092239
Luis Felipe <luis.s.mendes20@gmail.com>
2021-05-15 20:09:10 -0300

    Melhorando identacao

--- Viacep/ConsultaApi/ConsultaApi.cs
+++ Viacep/ConsultaApi/ConsultaApi.cs
@@ -6,22 +6,24 @@ namespace Viacep.ConsultaApi
 {
     public class ConsultaApi
     {
-        public void getViacep(string cep){
+        public void getViacep(string cep)
+        {
             string url = $"https://viacep.com.br/ws/{cep}/json/";
             using (HttpClient http = new HttpClient()) {
 
-                var response = http.GetAsync(url).GetAwaiter().GetResult();
-                var bodyResponse = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+            var response = http.GetAsync(url).GetAwaiter().GetResult();
+            var bodyResponse = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
 
-                if (response.StatusCode != System.Net.HttpStatusCode.OK)
-                {
-                    Console.WriteLine($"Cep digitado invalido!! \nOcep digitado foi :{cep}");
-                    return;
-                }
-                ViacepRetorno retornoCep = JsonConvert.DeserializeObject<ViacepRetorno>(bodyResponse);
-                Console.WriteLine($"seu Cep:{retornoCep.cep}\n" +
-                                  $"Rua:{retornoCep.logradouro}\n" +
-                                  $"Codigo IBGE: {retornoCep.ibge}");
+             if (response.StatusCode != System.Net.HttpStatusCode.OK)
+             {
+                 Console.WriteLine($"Cep digitado invalido!! \nOcep digitado foi :{cep}");
+                 return;
+             }
+
+             ViacepRetorno retornoCep = JsonConvert.DeserializeObject<ViacepRetorno>(bodyResponse);
+             Console.WriteLine($"seu Cep:{retornoCep.cep}\n" +
+                               $"Rua:{retornoCep.logradouro}\n" +
+                               $"Codigo IBGE: {retornoCep.ibge}");
             }
                
         }
