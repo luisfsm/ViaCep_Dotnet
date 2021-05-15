7349b403b2cb6840dde90ce9489f929314ad259a
Luis Felipe <luis.s.mendes20@gmail.com>
2021-05-15 20:20:45 -0300

    Ajustando identatacao

--- Viacep/ConsultaApi/ConsultaApi.cs
+++ Viacep/ConsultaApi/ConsultaApi.cs
@@ -9,25 +9,24 @@ namespace Viacep.ConsultaApi
         public void getViacep(string cep)
         {
             string url = $"https://viacep.com.br/ws/{cep}/json/";
-            using (HttpClient http = new HttpClient()) {
+            using (HttpClient http = new HttpClient())
+            {
+                var response = http.GetAsync(url).GetAwaiter().GetResult();
+                
 
-            var response = http.GetAsync(url).GetAwaiter().GetResult();
-            
+                 if (response.StatusCode != System.Net.HttpStatusCode.OK)
+                 {
+                     Console.WriteLine($"Cep digitado invalido!! \nO cep digitado foi :{cep}");
+                     return;
+                 }
 
-             if (response.StatusCode != System.Net.HttpStatusCode.OK)
-             {
-                 Console.WriteLine($"Cep digitado invalido!! \nO cep digitado foi :{cep}");
-                 return;
-             }
+                 var bodyResponse = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
 
-             var bodyResponse = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
-
-             ViacepRetorno retornoCep = JsonConvert.DeserializeObject<ViacepRetorno>(bodyResponse);
-             Console.WriteLine($"seu Cep:{retornoCep.cep}\n" +
-                               $"Rua:{retornoCep.logradouro}\n" +
-                               $"Codigo IBGE: {retornoCep.ibge}");
-            }
-               
+                 ViacepRetorno retornoCep = JsonConvert.DeserializeObject<ViacepRetorno>(bodyResponse);
+                 Console.WriteLine($"seu Cep:{retornoCep.cep}\n" +
+                                   $"Rua:{retornoCep.logradouro}\n" +
+                                   $"Codigo IBGE: {retornoCep.ibge}");
+             }         
         }
 
     }
