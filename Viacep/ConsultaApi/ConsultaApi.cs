c4266bb07fc96673f45d5796fc3dfa5561dc6d05
Luis Felipe <luis.s.mendes20@gmail.com>
2021-05-15 20:13:02 -0300

    alterando ordem de atribuicao de valores

--- Viacep/ConsultaApi/ConsultaApi.cs
+++ Viacep/ConsultaApi/ConsultaApi.cs
@@ -12,13 +12,15 @@ namespace Viacep.ConsultaApi
             using (HttpClient http = new HttpClient()) {
 
             var response = http.GetAsync(url).GetAwaiter().GetResult();
-            var bodyResponse = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+            
 
              if (response.StatusCode != System.Net.HttpStatusCode.OK)
              {
                  Console.WriteLine($"Cep digitado invalido!! \nO cep digitado foi :{cep}");
                  return;
              }
+
+             var bodyResponse = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
 
              ViacepRetorno retornoCep = JsonConvert.DeserializeObject<ViacepRetorno>(bodyResponse);
              Console.WriteLine($"seu Cep:{retornoCep.cep}\n" +
